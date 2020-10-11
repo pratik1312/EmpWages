fc500b1b582d822e3531b191c627cdfef67bcfd9
pratik1312 <patilpratik194@gmail.com>
2020-10-11 11:04:08 +0530

    Added:File calculates the daily wage for Part time employee

--- empwagecomputation.java
+++ empwagecomputation.java
@@ -2,12 +2,15 @@ public class empwagecomputation {
 	public static void main(String args[]) {
 		int WAGE_PER_HOUR = 20;
 		int FULL_DAY_HOUR = 8;
-		int IS_FULL_TIME =1;
+		int IS_PART_TIME = 2;
+		int IS_FULL_TIME = 1;
 		int empHrs = 0;
 		int empWage=0;
-		double empcheck = Math.floor(Math.random() * 10 ) % 2;
+		double empcheck = Math.floor(Math.random() * 10 ) % 3;
 		if (empcheck == IS_FULL_TIME)
 			empHrs=8;
+		else if (empcheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs=0;
 		empWage = empHrs * WAGE_PER_HOUR;
