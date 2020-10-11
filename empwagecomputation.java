5366927819fca6873cc2c4b049732caca6a49a83
pratik1312 <patilpratik194@gmail.com>
2020-10-11 12:15:39 +0530

    Added:File caclculate the employee wage till condition

--- empwagecomputation.java
+++ empwagecomputation.java
@@ -3,13 +3,17 @@ public class empwagecomputation {
 	public static final int IS_FULL_TIME = 1;
 	public static final int WAGE_PER_HOUR = 20;
 	public static final int NUM_OF_DAYS_IN_MONTH = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
 	public static void main(String args[]) {
 
 		int empHrs = 0;
 		int empWage=0;
 		int totalEmpWage = 0;
-		//int empcheck = (int) Math.floor(Math.random() * 10 ) % 3;
-		for (int day=0; day < NUM_OF_DAYS_IN_MONTH; day++) {
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_DAYS_IN_MONTH) {
+			totalWorkingDays++;
 			int empcheck = (int) Math.floor(Math.random() * 10 ) % 3;
 			switch (empcheck) {
 				case IS_FULL_TIME :
@@ -18,14 +22,16 @@ public class empwagecomputation {
 				case IS_PART_TIME :
 					empHrs = 4;
 					break;
-				default : 
+				default :
 					empHrs=0;
 				}
+		  totalEmpHrs += empHrs;
 		  empWage = empHrs * WAGE_PER_HOUR;
 		  totalEmpWage += empWage;
 		  System.out.println("Emp Wage : " + empWage);
 		  System.out.println("Total Employee Wage : " + totalEmpWage);
     	}
+		System.out.println("Total Working Hrs : " + totalEmpHrs);
 		System.out.println("Total Employee Wage : " + totalEmpWage);
     }
 }
